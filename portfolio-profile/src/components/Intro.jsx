f787718375031d5cf2b67edc81f01bc7404e36f7
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-01-27 16:48:08 +0800

    Improved Intro

--- portfolio-profile/src/components/Intro.jsx
+++ portfolio-profile/src/components/Intro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Intro(){
     return(
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+        <div className="pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl darl:text-white mb-1 md:mb-3 font-bold">Jb Villamor</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Computer Engineering Student</p>
             <p className="text-sm max-w-xl mb-6 font-bold">A hardworking and passionate job
