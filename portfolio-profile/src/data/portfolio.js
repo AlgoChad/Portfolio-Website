9fb723d26f52c9d638fbe4ad7d02ffa17488d318
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-01-27 13:20:17 +0800

    Initial

--- portfolio-profile/src/data/portfolio.js
+++ portfolio-profile/src/data/portfolio.js
@@ -12,7 +12,7 @@ export default [
         link: 'https://chat-app-five-eosin.vercel.app'
     },
     {
-        title: 'Cataract Detection System Augmented with Artificial Intelligence and Computer vision',
+        title: 'AI Cataract Detection System',
         imgUrl:'assets/Cataract.jpg',
         stack: ['Python', 'PyQt5', 'Raspberry Pi', 'OpenCv', 'TensorFlow', 'Keras'],
         link: 'https://chat-app-five-eosin.vercel.app'
