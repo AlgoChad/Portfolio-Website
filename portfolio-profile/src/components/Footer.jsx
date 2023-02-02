f45c87f35ef1eeff23415480c5f3b86f3a463c49
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-02-02 16:07:54 +0800

    Name edit

--- portfolio-profile/src/components/Footer.jsx
+++ portfolio-profile/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(){
     return(
-        <div className="py-5 text-center">
+        <div className="py-5 ">
             <p className='text-sm mt-2 opacity-50'>
                 &copy; {new Date().getFullYear()} Jhon Burn Villamor. All rights reserved.
             </p>
