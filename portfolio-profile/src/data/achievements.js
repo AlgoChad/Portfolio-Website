90a27c82e55820cda1fa1945866d2665207def56
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-03-08 01:40:12 +0800

    Update data in timelime

---
+++ portfolio-profile/src/data/achievements.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        year: '2023',
+        achievement: "National Level CpE Challenge C Programming 8th Place",
+        details: "ICpEP.SE/IRCCETE",
+    },
+    {
+        year: '2022',
+        achievement: "NCR Regional CpE Challenge C Programming 1st Runner Up",
+        details: "ICpEP.SE",
+    },
+    {
+        year: '2022',
+        achievement: "NCR CpE Challenge Idea Pitching Participant",
+        details: "ICpEP.SE",
+    },
+    {
+        year: '2022',
+        achievement: "Quiz Bee Participant",
+        details: "UP SquEEEze Quize Bee",
+    },
+    {
+        year: '2022',
+        achievement: "Quiz Bee Participant",
+        details: "UP SquEEEze Quize Bee",
+    },
+    {
+        year: '2022',
+        achievement: "3st Year Dean's Lister (with High Honors)",
+        details: "Colegio De San Lorenzo - College of Engineering",
+    },
+    {
+        year: '2020',
+        achievement: "1st Year Dean's Lister (with Honors)",
+        details: "Colegio De San Lorenzo - College of Engineering",
+    }
+]
