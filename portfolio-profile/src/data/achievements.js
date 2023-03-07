090f877de02dbd84f1a60a817a2db46bb4eed55e
AlgoChad <104708287+AlgoChad@users.noreply.github.com>
2023-03-08 02:07:05 +0800

    Update data in timelime2

--- portfolio-profile/src/data/achievements.js
+++ portfolio-profile/src/data/achievements.js
@@ -2,36 +2,43 @@ export default [
     {
         year: '2023',
         achievement: "National Level CpE Challenge C Programming 8th Place",
-        details: "ICpEP.SE/IRCCETE",
+        location: "ICpEP.SE/IRCCETE",
+        details: "As a National Level CpE Challenge participant, I competed in a rigorous programming competition that tested my knowledge and skills in C programming. Despite facing strong competition from other talented individuals, I was proud to achieve 8th place in the ICpEP.SE/IRCCETE competition. My success in this challenge is a testament to my strong understanding of programming concepts and my ability to apply them in real-world scenarios. Throughout the competition, I demonstrated my ability to write efficient and effective code that met the specific requirements of each challenge."
     },
     {
         year: '2022',
         achievement: "NCR Regional CpE Challenge C Programming 1st Runner Up",
-        details: "ICpEP.SE",
+        location: "ICpEP.SE",
+        details: " As the 1st Runner Up of the NCR Regional CpE Challenge in C Programming, I am proud to have competed against some of the most talented computer engineering students in the National Capital Region. This achievement demonstrates my expertise in C programming and my ability to apply it to real-world scenarios. Throughout the competition, I was tasked with solving complex programming problems that tested my knowledge of data structures, algorithms, and other fundamental programming concepts. Despite the intense competition, I was able to rise to the challenge and secure the 1st Runner Up spot in the ICpEP.SE competition."
     },
     {
         year: '2022',
         achievement: "NCR CpE Challenge Idea Pitching Participant",
-        details: "ICpEP.SE",
+        location: "ICpEP.SE",
+        details: ""
+    },
+    {
+        year: '2022',
+        achievement: "NCR CpE Challange Quiz Bowl Participant",
+        location: "ICpEP.SE",
+        details: ""
     },
     {
         year: '2022',
         achievement: "Quiz Bee Participant",
-        details: "UP SquEEEze Quize Bee",
-    },
-    {
-        year: '2022',
-        achievement: "Quiz Bee Participant",
-        details: "UP SquEEEze Quize Bee",
+        location: "UP SquEEEze Quize Bee",
+        details: ""
     },
     {
         year: '2022',
         achievement: "3st Year Dean's Lister (with High Honors)",
-        details: "Colegio De San Lorenzo - College of Engineering",
+        location: "Colegio De San Lorenzo - College of Engineering",
+        details: ""
     },
     {
         year: '2020',
         achievement: "1st Year Dean's Lister (with Honors)",
-        details: "Colegio De San Lorenzo - College of Engineering",
+        location: "Colegio De San Lorenzo - College of Engineering",
+        details: ""
     }
 ]
